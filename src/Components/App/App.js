6b519361cebd14e147fa8c257a780d8c2e063dcf
Honorio <htaveras90@gmail.com>
2021-01-29 22:11:05 -0700

    logging data with hardcoded state vals, downloaded airbnb style guide

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,24 @@
-function App() {
-  return (
-  <div>Sup dirtbag!</div>
-  );
-}
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [searchItem, setSearchItem] = useState('jeans');
+  const [pageNumber, setPageNumber] = useState(2);
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.searchspring.net/api/search/search.json?siteId=scmq7n&q=${searchItem}&resultsFormat=native&page=${pageNumber}`,
+      )
+      .then(
+        (res) => console.log(res.data),
+        (err) => {
+          console.log('search error: ', JSON.parse(err));
+        },
+      );
+  }, [searchItem, pageNumber]);
+
+  return <div>Sup dirtbag!</div>;
+};
 
 export default App;
