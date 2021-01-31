d1b085ba936c28b3ef4f5248ff30871b286ef2a3
Honorio <htaveras90@gmail.com>
2021-01-30 23:20:12 -0700

    finished conditional rendering of msrp and price logic in item component

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 const Item = ({ image, name, msrp, price }) => (
   <div>
     <img src={image} alt='' />
-    <p className="item-name">{name}</p>
-    {/* TODO: conditional rendering for price */}
-    <span className="msrp">${msrp}</span>
-    <span className="price">${price}</span>
+    <p className='item-name'>{name}</p>
+    {msrp && msrp * 1 > price * 1 ? (
+      <>
+        <span className='msrp-crossed'>${msrp}</span>
+        <span className='price'>${price}</span>
+      </>
+    ) : (
+      <span className='msrp'>${msrp}</span>
+    )}
   </div>
 );
 
