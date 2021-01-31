b55500cf185f71545d670db7900aae7bf034f49b
Honorio <htaveras90@gmail.com>
2021-01-30 19:43:36 -0700

    continuing logic on conditional rendering for pagination buttons

--- src/Components/PaginateResults/PaginateResults.jsx
+++ src/Components/PaginateResults/PaginateResults.jsx
@@ -3,16 +3,36 @@ import React from 'react';
 import { Pagination } from 'react-bootstrap';
 
 const PaginateResults = ({
-  pagination: { begin, end, nextPage, previousPage, currentPage },
+  pagination: { begin, totalPages, nextPage, previousPage, currentPage },
   paginate,
 }) => {
+  console.log(
+    'begin: ',
+    begin,
+    ' previousPage: ',
+    previousPage,
+    ' currentPage: ',
+    currentPage,
+    ' nextPage: ',
+    nextPage,
+    ' totalPages: ',
+    totalPages
+  );
   return (
     <>
       <Pagination>
-        <Pagination.First onClick={() => paginate(1)} />
-        <Pagination.Prev onClick={() => paginate(previousPage)} />
-        <Pagination.Next onClick={() => paginate(nextPage)} />
-        <Pagination.Last onClick={() => paginate(end)} />
+        {currentPage > 1 ? (
+          <>
+            <Pagination.First onClick={() => paginate(1)} />
+            <Pagination.Prev onClick={() => paginate(previousPage)} />
+          </>
+        ) : null}
+        {currentPage < totalPages ? (
+          <>
+            <Pagination.Next onClick={() => paginate(nextPage)} />
+            <Pagination.Last onClick={() => paginate(totalPages)} />
+          </>
+        ) : null}
       </Pagination>
     </>
   );
