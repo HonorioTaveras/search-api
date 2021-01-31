ac1617bde8d9654b9376556a1d0de282ee82db98
Honorio <htaveras90@gmail.com>
2021-01-30 22:46:32 -0700

    pagintion works as user stories require

--- src/Components/PaginateResults/PaginateResults.jsx
+++ src/Components/PaginateResults/PaginateResults.jsx
@@ -3,31 +3,42 @@ import React from 'react';
 import { Pagination } from 'react-bootstrap';
 
 const PaginateResults = ({
-  pagination: { begin, totalPages, nextPage, previousPage, currentPage },
+  pagination: { totalPages, nextPage, previousPage, currentPage },
   paginate,
 }) => {
-  console.log(
-    'begin: ',
-    begin,
-    ' previousPage: ',
-    previousPage,
-    ' currentPage: ',
-    currentPage,
-    ' nextPage: ',
-    nextPage,
-    ' totalPages: ',
-    totalPages
-  );
+  const showPages = () => {
+    let pages = [];
+
+    if (currentPage >= 3 && currentPage < totalPages - 5) {
+      for (let i = currentPage - 2; i <= currentPage + 2; i += 1) {
+        pages.push(<Pagination.Item key={i}>{i}</Pagination.Item>);
+      }
+    }
+    if (currentPage < 3) {
+      for (let i = 1; i <= 5; i += 1) {
+        pages.push(<Pagination.Item key={i}>{i}</Pagination.Item>);
+      }
+    }
+    if (currentPage >= totalPages - 5) {
+      for (let i = totalPages - 4; i <= totalPages; i += 1) {
+        pages.push(<Pagination.Item key={i}>{i}</Pagination.Item>);
+      }
+    }
+
+    return pages;
+  };
+
   return (
     <>
       <Pagination>
-        {currentPage > 1 ? (
+        {currentPage > 1 && nextPage > 0 ? (
           <>
             <Pagination.First onClick={() => paginate(1)} />
             <Pagination.Prev onClick={() => paginate(previousPage)} />
           </>
         ) : null}
-        {currentPage < totalPages ? (
+        {nextPage > 0 ? showPages().map((page) => page) : null}
+        {currentPage < totalPages && nextPage > 0 ? (
           <>
             <Pagination.Next onClick={() => paginate(nextPage)} />
             <Pagination.Last onClick={() => paginate(totalPages)} />
