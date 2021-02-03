0e5d60dd96f36187fcf35087e3d42000c1a82387
Honorio <htaveras90@gmail.com>
2021-02-03 09:52:01 -0700

    added broken image to item component in case of error loading src image

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -6,18 +6,28 @@ import Placeholder from '../../assets/photo.png';
 
 import './Item.scss';
 
-const Item = ({ image, name, msrp, price }) => (
-  <div className="item-container">
+const Item = ({ image, name, msrp, price }) => {
+  let errorFlag = true;
+
+  const handleError = (e) => {
+    if (errorFlag) {
+      errorFlag = false;
+      e.target.src = Placeholder;
+    }
+  }
+
+  return (
+  <div className='item-container'>
     <LazyLoadImage
-      className="image"
+      className='image'
       alt=''
       src={image}
-      placeholderSrc={Placeholder}
       effect='blur'
+      onError={handleError}
     />
     <p className='item-name'>{name}</p>
     {msrp && msrp * 1 > price * 1 ? (
-      <div className="crossed">
+      <div className='crossed'>
         <span className='msrp-crossed'>${msrp}</span>
         <span className='price'>${price}</span>
       </div>
@@ -25,6 +35,6 @@ const Item = ({ image, name, msrp, price }) => (
       <span className='msrp'>${msrp}</span>
     )}
   </div>
-);
+)};
 
 export default Item;
